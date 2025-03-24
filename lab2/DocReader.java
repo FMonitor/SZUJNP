25113a85ff41355d27854c534672358bfa854149
LCMonitor <2023152031@email.szu.edu.cn>
2025-03-24 17:14:15 +0800

    changed Reader

--- lab2/DocReader.java
+++ lab2/DocReader.java
@@ -5,7 +5,9 @@ import java.io.InputStreamReader;
 public class DocReader {
     public static void main(String[] args) {
         try {
-            BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream("numbers.txt")));
+            BufferedReader br = new BufferedReader(
+                    new InputStreamReader(
+                    new FileInputStream("numbers.txt")));
             String line = null;
             while ((line = br.readLine()) != null) {
                 line = line.replace(" ", "");
