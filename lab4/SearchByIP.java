8ac706ff8b5291e8400715bca841b4275313a020
FMonitor <13723423785@163.com>
2025-04-21 13:23:16 +0800

    change lab4/SearchByIP.java

--- lab4/SearchByIP.java
+++ lab4/SearchByIP.java
@@ -1,15 +1,20 @@
 import java.net.InetAddress;
+import java.util.Scanner;
 
 public class SearchByIP {
     public static void main(String[] args) {
         try {
-            InetAddress szuAddress = InetAddress.getByName("dns.google.com");
-            System.out.println("主机名对应的IP地址: " + szuAddress.getHostAddress());
-
-            InetAddress szuIPAddress = InetAddress.getByName("8.8.8.8");
-            System.out.println("IP地址对应的主机名: " + szuIPAddress.getCanonicalHostName());
-
-             
+            Scanner scanner = new Scanner(System.in);
+            System.out.print("请输入要查询的IP地址或主机名: ");
+            String ipAddress = scanner.nextLine();
+            InetAddress inetAddress = InetAddress.getByName(ipAddress);
+            boolean isIP = inetAddress.getHostAddress().equals(ipAddress);
+            if (isIP) {
+                System.out.println("IP地址对应的主机名: " + inetAddress.getHostName());
+            } else {
+                System.out.println("主机名对应的IP地址: " + inetAddress.getHostAddress());
+            }
+            scanner.close();
         } catch (Exception e) {
             e.printStackTrace();
         }
